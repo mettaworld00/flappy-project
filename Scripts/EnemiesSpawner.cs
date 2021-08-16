7deffbb25e7eb0582882c4a0e76a688677ce2444
mettaworld00 <mettaworldfit@gmail.com>
2021-08-16 09:14:42 -0400

    hola

---
+++ Scripts/EnemiesSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+
+    public float tiempoMax = 1;
+    public float tiempo = 0;
+    public GameObject mace;
+    public float altura;
+    public float distanciaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (tiempo > tiempoMax)
+        {
+            GameObject nuevomace = Instantiate(mace);
+            nuevomace.transform.position = transform.position + new Vector3(distanciaX, Random.Range(-altura, altura), 0);
+            Destroy(nuevomace, 15);
+
+            tiempo = 0;
+        }
+        tiempo += Time.deltaTime;
+
+    }
+}
